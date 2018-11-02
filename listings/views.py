8ab08ed26ebe6b2238d47debe456a3e6f3090953
wencakisa <wencakisa@gmail.com>
2018-11-02 20:12:25 +0200

    Added raw listing view set

--- listings/views.py
+++ listings/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from .models import Listing
+from .serializers import ListingSerializer
+
+
+class ListingViewSet(viewsets.ModelViewSet):
+    serializer_class = ListingSerializer
+    queryset = Listing.objects.all()
+
+    # TODO: Implement permissions and add permission_classes
+
+    def perform_create(self, serializer):
+        serializer.save(resident=self.request.user)
