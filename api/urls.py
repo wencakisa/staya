83555d8cace4bc4347d0dec4799e020f8cc3fb08
wencakisa <wencakisa@gmail.com>
2018-11-03 00:09:55 +0200

    Exposed docs for easier client development

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,14 @@
 from django.urls import path, include
 
+from rest_framework.documentation import include_docs_urls
+
+
 urlpatterns = [
+    path('docs/', include_docs_urls(
+        title='Staya API Docs',
+        authentication_classes=[],
+        permission_classes=[]
+    )),
     path('auth/', include('rest_auth.urls')),
     path('auth/registration/', include('rest_auth.registration.urls')),
     path('', include('listings.urls')),
