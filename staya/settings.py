99b0a743383de6442f5d08f8a526eba04cceff83
wencakisa <wencakisa@gmail.com>
2018-11-02 19:12:02 +0200

    Registered api and users apps and set global auth user model to the custom one

--- staya/settings.py
+++ staya/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Project apps
+    'api',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -118,3 +122,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# Custom User Model
+
+AUTH_USER_MODEL = 'users.User'
