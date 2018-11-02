5f7a04707aee5e4d4bae2e1fc833f7b5260f89d1
wencakisa <wencakisa@gmail.com>
2018-11-02 22:17:01 +0200

    Installed drf-nested-routers in order to expose booking endpoints as a nested resource to listings

--- listings/urls.py
+++ listings/urls.py
@@ -1,13 +1,18 @@
 from django.urls import path, include
 
 from rest_framework import routers
+from rest_framework_nested import routers as nested_routers
 
-from listings.views import ListingViewSet
+from listings.views import ListingViewSet, BookingViewSet
 
 
 listings_router = routers.SimpleRouter()
 listings_router.register('listings', ListingViewSet)
 
+bookings_router = nested_routers.NestedSimpleRouter(listings_router, 'listings', lookup='listing')
+bookings_router.register('bookings', BookingViewSet, base_name='listing-bookings')
+
 urlpatterns = [
     path('', include(listings_router.urls), name='listings'),
+    path('', include(bookings_router.urls), name='listing-bookings'),
 ]
