df87d840a86972f95d450149ee4a984bcd671784
wencakisa <wencakisa@gmail.com>
2018-11-02 22:00:54 +0200

    Registered location in admin

--- listings/admin.py
+++ listings/admin.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 
-from .models import Amenity, Listing
+from .models import Location, Amenity, Listing
 
 
 class ListingInline(admin.StackedInline):
     model = Listing
 
 
+admin.site.register(Location)
 admin.site.register(Amenity)
 admin.site.register(Listing)
