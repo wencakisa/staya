863cce9069e9a3989a04a1ef9ec540c006258f58
wencakisa <wencakisa@gmail.com>
2018-11-03 16:25:11 +0200

    Reusing logic from IsResidentUser permission

--- listings/permissions.py
+++ listings/permissions.py
@@ -8,14 +8,14 @@ class IsResidentUser(permissions.BasePermission):
         return request.user.is_resident
 
 
-class ListingCreatingPermission(permissions.BasePermission):
+class ListingCreatingPermission(IsResidentUser):
     message = 'You have to be a resident user in order to create listings.'
 
     def has_permission(self, request, view):
         if request.method in permissions.SAFE_METHODS:
             return True
 
-        return request.user.is_resident
+        return super().has_permission(request, view)
 
 
 class ListingModifyingPermission(permissions.BasePermission):
