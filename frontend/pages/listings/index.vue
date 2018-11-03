2f07301803c458d7e7faeb841c52bbed0def9316
matir8 <martin.markov28@gmail.com>
2018-11-03 11:39:12 +0200

    Add listing page

--- frontend/pages/listings/index.vue
+++ frontend/pages/listings/index.vue
@@ -27,8 +27,16 @@
             </div>
           </div>
         </div>
+
       </div>
     </transition-group >
+    <nuxt-link
+      v-if="$store.getters.isResident"
+      class="btn-circle btn-lg has-background-dark tooltip"
+      data-tooltip="Add Listing"
+      to="/listings/add">
+      <i class="mdi mdi-plus"/>
+    </nuxt-link>
   </div>
 </template>
 
@@ -37,43 +45,74 @@ export default {
   data() {
     return {
       listings: []
-    }
+    };
   },
   mounted() {
-    let url = this.$route.query.search ? `/listings?search=${this.$route.query.search}` : '/listings'
-    return this.$axios.get(url)
-    .then((res) => {
-      this.listings = res.data
-    })
+    let url = this.$route.query.search
+      ? `/listings?search=${this.$route.query.search}`
+      : "/listings";
+    return this.$axios.get(url).then(res => {
+      this.listings = res.data;
+    });
   }
-
-}
+};
 </script>
 
 <style>
-  .container {
-    margin-top: 2%;
-  }
-  .card {
-    border: 15px solid white;
-    border-radius: 5px;
-    cursor: pointer;
-  }
-  .card:hover {
-    -webkit-filter:drop-shadow(8px 8px 10px black);
-    -moz-filter:drop-shadow(8px 8px 10px black);
-    filter:drop-shadow(8px 8px 10px black);
-    transition: all 0.5s ease;
-  }
-  .list-item {
-    display: inline-block;
-    margin-right: 10px;
-  }
-  .list-enter-active, .list-leave-active {
-    transition: all 1s;
-  }
-  .list-enter, .list-leave-to {
-    opacity: 0;
-    transform: translateY(30px);
-  }
+.container {
+  margin-top: 2%;
+}
+.card {
+  border: 15px solid white;
+  border-radius: 5px;
+  cursor: pointer;
+}
+.card:hover {
+  -webkit-filter: drop-shadow(8px 8px 10px black);
+  -moz-filter: drop-shadow(8px 8px 10px black);
+  filter: drop-shadow(8px 8px 10px black);
+  transition: all 0.5s ease;
+}
+.list-item {
+  display: inline-block;
+  margin-right: 10px;
+}
+.list-enter-active,
+.list-leave-active {
+  transition: all 1s;
+}
+.list-enter, .list-leave-to /* .list-leave-active below version 2.1.8 */ {
+  opacity: 0;
+  transform: translateY(30px);
+}
+
+.btn-circle.btn-lg {
+    position: fixed;
+    z-index: 1;
+    right: 0.5em;
+    bottom: 2em;
+    border: none;
+    border-radius: 50%;
+    height: 60px;
+    width: 60px;
+    padding: 10px 16px;
+    font-size: 20px;
+    line-height: 1.33;
+}
+
+.btn-circle i {
+    font-size: 28px;
+    color: white;
+}
+
+.btn-circle i:hover {
+    color: gold;
+}
+
+.tooltip::after{
+  padding: 0rem;
+  box-shadow: none;
+  -webkit-box-shadow: none;
+  border-radius: 0px;
+}
 </style>
