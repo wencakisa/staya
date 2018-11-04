a94b11194ff8feb45b4f03c84413e9755c68a5ee
wencakisa <wencakisa@gmail.com>
2018-11-04 10:37:11 +0200

    Listing unbooked permission in process...

---
+++ listings/permissions.py
@@ -0,0 +1,58 @@
+from rest_framework import permissions
+
+
+class IsCurrentUser(permissions.BasePermission):
+    message = 'You can view only your own bookings.'
+
+    def has_object_permission(self, request, view, obj):
+        return obj.user == request.user
+
+
+class IsResidentUser(permissions.BasePermission):
+    message = 'You have to be a resident user in order to access this view.'
+
+    def has_permission(self, request, view):
+        return request.user.is_resident
+
+
+class ListingCreatingPermission(IsResidentUser):
+    message = 'You have to be a resident user in order to create listings.'
+
+    def has_permission(self, request, view):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        return super().has_permission(request, view)
+
+
+class ListingModifyingPermission(permissions.BasePermission):
+    message = 'You can only modify your own listings.'
+
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        return obj.resident == request.user
+
+
+class BaseNestedListingResourcePermission(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        return obj.resident != request.user
+
+
+class ListingBookingPermission(BaseNestedListingResourcePermission):
+    message = 'You can not book your own listings.'
+
+
+class ListingReviewPermission(BaseNestedListingResourcePermission):
+    message = 'You can not review your own listings.'
+
+
+# class ListingIsUnbookedYet(permissions.BasePermission):
+#     message = 'Listing is booked.'
+
+#     def has_object_permission(self, request, view, obj):
+#         return obj.is_unbooked(request.data['check_in'], request.data['check_out'])
---
+++ listings/views.py
@@ -0,0 +1,87 @@
+from rest_framework import viewsets, filters, generics
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
+
+from .models import Amenity, Listing, Booking, Review
+from .serializers import (
+    AmenitySerializer,
+    ListingSerializer,
+    BookingSerializer,
+    ReviewSerializer,
+    UserBookingSerializer
+)
+from .permissions import (
+    IsCurrentUser,
+    IsResidentUser,
+    ListingCreatingPermission,
+    ListingModifyingPermission,
+    ListingBookingPermission,
+    ListingReviewPermission,
+    # ListingIsUnbookedYet
+)
+from .filters import ListingsFreeDateFilter, NearbyListingsFilter
+
+
+class AmenitiesList(generics.ListAPIView):
+    serializer_class = AmenitySerializer
+    queryset = Amenity.objects.all()
+    permission_classes = (IsAuthenticated, IsResidentUser)
+
+
+class ListingViewSet(viewsets.ModelViewSet):
+    serializer_class = ListingSerializer
+    queryset = Listing.objects.all()
+    permission_classes = (
+        # Listings should be availbable for anonymous users too
+        IsAuthenticatedOrReadOnly,
+        ListingCreatingPermission,
+        ListingModifyingPermission
+    )
+    filter_backends = (filters.SearchFilter, NearbyListingsFilter, ListingsFreeDateFilter)
+    search_fields = ('title', 'guest_amount', 'location__title', 'resident__username')
+
+    def perform_create(self, serializer):
+        serializer.save(resident=self.request.user)
+
+
+class BookingViewSet(viewsets.ModelViewSet):
+    serializer_class = BookingSerializer
+    permission_classes = (IsAuthenticated, ListingBookingPermission)
+
+    def get_queryset(self):
+        return Booking.current_bookings().filter(listing=self.kwargs['listing_pk'])
+
+    def perform_create(self, serializer):
+        user = self.request.user
+        listings = Listing.objects.all()
+
+        listing = generics.get_object_or_404(listings, id=self.kwargs['listing_pk'])
+
+        self.check_object_permissions(self.request, listing)
+
+        serializer.save(user=user, listing=listing)
+
+
+class UserBookings(generics.ListAPIView):
+    serializer_class = UserBookingSerializer
+    permission_classes = (IsAuthenticated, IsCurrentUser)
+
+    def get_queryset(self):
+        return Booking.objects.filter(user=self.request.user)
+
+
+class ReviewViewSet(viewsets.ModelViewSet):
+    serializer_class = ReviewSerializer
+    permission_classes = (IsAuthenticated, ListingReviewPermission)
+
+    def get_queryset(self):
+        return Review.objects.filter(listing=self.kwargs['listing_pk'])
+
+    def perform_create(self, serializer):
+        user = self.request.user
+        listings = Listing.objects.all()
+
+        listing = generics.get_object_or_404(listings, id=self.kwargs['listing_pk'])
+
+        self.check_object_permissions(self.request, listing)
+
+        serializer.save(user=user, listing=listing)
